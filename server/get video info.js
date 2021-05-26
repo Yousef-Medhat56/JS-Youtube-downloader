d834bc681266c48cb640a1ac9677b803464f4600
Yousef-Medhat56 <yousif.medhat562004@gmail.com>
2021-05-26 19:19:12 +0200

    throw error if the video url is unvalid

--- server/get video info.js
+++ server/get video info.js
@@ -7,7 +7,9 @@ const youtubeDl = require("ytdl-core")
 const getVideoInfo = async(videoUrl) => {
     return youtubeDl.getInfo(videoUrl) //get the video info
         .then(data => { return data }) //assign the video info to the function
+        .catch(err => { new Error("wrong url") }) //throw error if the video url is unvalid
 }
+
 
 //exports (getVideoInfo) function
 module.exports = getVideoInfo

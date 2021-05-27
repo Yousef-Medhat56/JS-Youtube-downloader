5a5706af55d515594e849cf860e6964418f3eb28
Yousef-Medhat56 <yousif.medhat562004@gmail.com>
2021-05-27 09:20:46 +0200

    handle errors that may happen if the user clicked cumbit button with empty input field or unvalid video url

--- public/JavaScript/fetch APi.js
+++ public/JavaScript/fetch APi.js
@@ -3,12 +3,30 @@ const videoUrlForm = document.getElementById("video-url-form") //form tag
 const videoUrlInput = document.getElementById("video-url-input") //input video link field
 const videoUrlBtn = document.getElementById("video-url-btn") //submit button
 
-//add click event to the submit button
-videoUrlBtn.onclick = (e) => {
+//define the function that will be invoked after clicking the submit button
+const clickSubmitBtn = e => {
     e.preventDefault()
 
-    //fetch video data
-    fetch(`http://localhost:7777/getData/?videoUrl=${videoUrlInput.value}`)
-        .then(response => response.json())
-        .then(data => console.log(data))
+    //check if the input field is empty
+    if (!videoUrlInput.value) {
+        showErrorMsg("Please enter video url") //ask the user to enter a video url
+    }
+    //if the input field isn't empty
+    else {
+        //fetch video data
+        fetch(`http://localhost:7777/getData/?videoUrl=${videoUrlInput.value}`)
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(err => { //if there is an error
+                showErrorMsg("Please enter valid video url") //ask the user to enter the url again
+                ctrlLoaderDis("block", "none") //hide loader and show submit button again
+            })
+
+
+        //show loader and hide submit button to prevent the user from clicking on the button again during fecthing the video info
+        ctrlLoaderDis("none", "flex")
+    }
 }
+
+//add click event to the submit button
+videoUrlBtn.addEventListener("click", clickSubmitBtn)

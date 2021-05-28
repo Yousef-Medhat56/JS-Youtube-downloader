537d18e8a7c553434bb6962bf78dcd171f16bbe6
Yousef-Medhat56 <yousif.medhat562004@gmail.com>
2021-05-28 09:31:18 +0200

    define function that controls the appearance of video duration

--- public/JavaScript/show video info.js
+++ public/JavaScript/show video info.js
@@ -5,6 +5,33 @@ const videoUrlForm = document.getElementById("video-url-form") //form tag
 const frontSide = document.getElementById("front-side") //the front side that contain the input field and submit button
 const backSide = document.getElementById("back-side") //The back side that will contain the video details
 
+/*video duration value in the API is in seconds only,
+so this function convert video duration into seconds, minutes and hours */
+const showVidDuration = (videoDuration) => {
+    const minutes = Math.floor(videoDuration / 60) //video duration > minutes
+    const seconds = videoDuration - (minutes * 60) //video duration > seconds
+
+    const writeZero = (duration) => {
+        if (duration > 9) return duration //check if the duration value is bigger than 9
+            //if the duration value is less than 9
+        return `0${duration}` //write 0 before the duration
+    }
+
+    //if the video is less than one minute
+    if (minutes === 0) return `00:${writeZero(videoDuration)}`
+
+    //if the video is less than one hour
+    else if (minutes > 0 && minutes < 60) return `${writeZero(minutes)}:${writeZero(seconds)}`
+
+    //if the video is bigger than one hour
+    else if (minutes > 59) {
+        const hours = Math.floor(minutes / 60)
+        const minutes2 = minutes - (hours * 60) //amount of minutes after sunbstracting hours from it
+        return `${writeZero(hours)}:${writeZero(minutes2)}:${writeZero(seconds)}`
+    }
+}
+
+
 //rotate front and back sides after clicking submit button
 const rotateSides = () => {
     frontSide.style.transform = "perspective(2000px) rotateY(180deg)" //rotate front side
@@ -34,7 +61,7 @@ const showVideoInfo = (videoInfo) => {
     <div id= "video-info">
     <h3 id= "video-title">${title}</h3>
     <h4 id = "channel-name">${ownerChannelName}</h4>
-    <div id= "video-duration">${lengthSeconds}</div>
+    <h4 id= "video-duration">${showVidDuration(parseInt(lengthSeconds))}</h4>
     <div>
     <h4 id= "video-views">${viewCount} views . ${uploadDate}</h4>
     
